3faab8ac24b9b73849bc8e2722890d81a01ad5e7
letu05 <148754162+letu05@users.noreply.github.com>
2026-02-05 07:55:53 +0700

    update

---
+++ chapter1/Ex1/Test.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter1.Ex1;
+
+/**
+ *
+ * @author tu650
+ */
+public class Test<T> {
+    private T value;
+    public void set (T value){
+        this.value = value;
+    }
+    public T get(){
+        return value;
+    }
+    
+    
+}
--- chapter1/Ex2/Main.java
+++ chapter1/Ex2/Main.java
@@ -2,20 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package chapter1;
+package chapter1.Ex2;
 
 /**
  *
  * @author tu650
  */
-public class Test<T> {
-    private T value;
-    public void set (T value){
-        this.value = value;
+public class Main {
+    public static <E> void printArry(E[] array){
+        for(E element : array){
+            System.out.println(element +"");
+            
+        }
+        System.out.println();
     }
-    public T get(){
-        return value;
-    }
-    
-    
 }
